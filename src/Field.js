808c40208add828d6ef964026f12531b5200a525
Kiryl Arlouski <Kiryl.Arlouski@itechart-group.com>
2018-08-08 11:50:11 +0300

    add array variant of value prop in Field component

--- src/Field.js
+++ src/Field.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Field = ({ label = '', value = '' }) => {
-    return <div className="row">
+    return <div className="row py-2">
         <div className="col-4">{label}</div>
-        <div className="col-8">{value}</div>
+        { 
+            Array.isArray(value)
+                ? <div className="col-8" >{value.map(item => <div className="mb-2 border-bottom" key={item}>{item}</div>)}</div> 
+                : <div className="col-8 border-bottom">{value}</div>
+        }
     </div>
-}
+};
+
+Field.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.string),
+        PropTypes.string,
+    ]),
+};
 
 export default Field;
